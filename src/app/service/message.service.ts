6209885679a0f60f51186c6cec9fd821c655354c
T1Sent0 <foregestore@gmail.com>
2020-03-03 10:09:09 +0300

    Update message.service.ts

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class MessageService {
-
+  // инициализация в конструкторе
   public messages: string[] = [];
 
   public add(message: string) {
