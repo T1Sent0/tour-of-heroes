a641d96923dba643da3083e36fc775b8ed0d33cf
T1Sent0 <foregestore@gmail.com>
2020-03-03 09:53:24 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // инициализация в конструкторе
   public title: string = 'Tour of Heroes';
 }
